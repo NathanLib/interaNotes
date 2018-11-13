432e20cfbf9696aa379de1cdaac0044fb362f0bb
Leger Charlie <charlie.leger@etu.unilim.fr>
2018-11-13 23:18:19 +0100

    preparation test_afficherSujet

---
+++ interaNotes/include/pages/test_afficherUnSujetComplet.inc.php
@@ -0,0 +1,8 @@
+<!--Remplacer chaque valeur par des données provenant de la bd (jeu d'essai)-->
+<div id="sujet">
+  <p>Titre : XXX</p>
+  <p>Date de fin : jj/mm/yyyy
+  <p>Consignes : Lorem Ipsum</p>
+  <p>Question : questions à poser</p>
+  <p>Valeurs attribuées au sujet</p>
+</div>
