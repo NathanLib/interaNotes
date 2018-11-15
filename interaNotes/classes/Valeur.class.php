fa69137422c5b5246344639ccc77395aa3d9958f
Leger Charlie <charlie.leger@etu.unilim.fr>
2018-11-15 14:16:00 +0100

    add class point+valeur

---
+++ interaNotes/classes/Valeur.class.php
@@ -0,0 +1,62 @@
+<?php
+class Valeur{
+	private $idValeur;
+	private $idPoint;
+  private $valeur;
+
+	public function __construct($valeurs){
+		if(!empty($valeurs)){
+			$this->affecte($valeurs);
+		}
+	}
+
+	public function affecte($valeurs){
+		foreach($valeurs as $attribut => $valeur){
+
+			switch($attribut){
+				case 'idValeur' :
+					$this->setIdValeur($valeur);
+					break;
+
+				case 'idPoint' :
+					$this->setIdPointOfValeur($valeur);
+					break;
+
+        case 'valeur' :
+					$this->setValeur($valeur);
+					break;
+			}
+		}
+	}
+
+	public function getIdValeur(){
+		return $this->idValeur;
+	}
+
+	public function setIdValeur($id){
+		if(is_numeric($id)){
+			$this->idValeur = $idValeur;
+		}
+	}
+
+	public function getIdPointOfValeur(){
+		return $this->idPoint;
+	}
+
+	public function setIdPointOfValeur($idPoint){
+    if(is_numeric($idPoint)){
+		    $this->idPoint = $idPoint;
+    }
+	}
+
+  public function getValeur(){
+		return $this->valeur;
+	}
+
+	public function setValeur($valeur){
+    if(is_numeric($valeur)){
+			$this->valeur = $valeur;
+		}
+	}
+
+}
