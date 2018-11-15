7e9f33ee17616786bf18894e7a42a0192884c1d1
Kant1 <quentin.lenoir@etu.unilim.fr>
2018-11-15 15:29:08 +0100

    fix Valeur

--- interaNotes/classes/ValeurManager.class.php
+++ interaNotes/classes/ValeurManager.class.php
@@ -21,4 +21,64 @@ class ValeurManager{
 		$requete->closeCursor();
 		return $listeValeurs;
 	}
+
+	public function getValeurSujet($idSujet){
+
+		$sql = 'SELECT idValeur FROM exercicegenere e WHERE e.idSujet=:idSujet';
+
+		$requete = $this->db->prepare($sql);
+		$requete->bindValue(':idSujet', $idSujet);
+		$requete->execute();
+
+		while($valeur = $requete->fetch(PDO::FETCH_OBJ)){
+			$listeValeur[] = new Valeur($valeur);
+		}
+
+		$requete->closeCursor();
+
+		return $listeValeur;
+	}
+
+	public function getPointFromValeur($idValeur){
+
+		$sql = 'SELECT idPoint FROM valeurs WHERE idValeur=:idValeur';
+
+		$requete = $this->db->prepare($sql);
+		$requete->bindValue(':idValeur', $idValeur);
+		$requete->execute();
+
+		$idPoint = $requete->fetch(PDO::FETCH_OBJ);
+
+		$requete->closeCursor();
+
+		return $idPoint->idPoint;
+	}
+
+	public function getPoint($idPoint){
+		$sql = 'SELECT * FROM points WHERE idPoint=:idPoint';
+
+		$requete = $this->db->prepare($sql);
+		$requete->bindValue(':idPoint', $idPoint);
+		$requete->execute();
+
+		$point = $requete->fetch(PDO::FETCH_OBJ);
+
+		$requete->closeCursor();
+
+		return $point;
+	}
+
+	public function getValeur($idValeur){
+		$sql = 'SELECT valeur FROM valeurs WHERE idValeur=:idValeur';
+
+		$requete = $this->db->prepare($sql);
+		$requete->bindValue(':idValeur', $idValeur);
+		$requete->execute();
+
+		$valeur = $requete->fetch(PDO::FETCH_OBJ);
+
+		$requete->closeCursor();
+
+		return $valeur->valeur;
+	}
 }
