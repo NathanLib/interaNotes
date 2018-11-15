f893f34c77c4f2cac6928b3ad14a7772b41b190a
Leger Charlie <charlie.leger@etu.unilim.fr>
2018-11-15 14:48:39 +0100

    add ValeurManager

---
+++ interaNotes/classes/ValeurManager.class.php
@@ -0,0 +1,24 @@
+<?php
+class ValeurManager{
+	private $db;
+
+	public function __construct($db){
+		$this->db = $db;
+	}
+
+	public function getAllValeursOfPoints($idPoint){
+
+		$sql = 'SELECT idValeur, idPoint, valeur FROM valeurs WHERE idPoint=:idPoint';
+
+    $requete = $this->db->prepare($sql);
+		$requete->bindValue(':idPoint', $idPoint);
+		$requete->execute();
+
+		while($valeur = $requete->fetch(PDO::FETCH_OBJ)){
+			$listeValeurs[] = new Valeur($valeur);
+		}
+
+		$requete->closeCursor();
+		return $listeValeurs;
+	}
+}
