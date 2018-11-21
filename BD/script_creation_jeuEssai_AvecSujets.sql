9b93142f70b8fbc29c2c9e8a72798a41501c1d10
Leger Charlie <charlie.leger@etu.unilim.fr>
2018-11-21 14:06:59 +0100

    add jeu essai avec sujets

---
+++ BD/script_creation_jeuEssai_AvecSujets.sql
@@ -0,0 +1,403 @@
+-- MySQL dump 10.13  Distrib 5.7.19, for Win64 (x86_64)
+--
+-- Host: localhost    Database: intera_notes
+-- ------------------------------------------------------
+-- Server version	5.7.19
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `eleve`
+--
+
+DROP TABLE IF EXISTS `eleve`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `eleve` (
+  `idEleve` int(11) NOT NULL,
+  `annee` int(4) NOT NULL,
+  PRIMARY KEY (`idEleve`),
+  CONSTRAINT `eleve_ibfk_1` FOREIGN KEY (`idEleve`) REFERENCES `personne` (`idPersonne`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `eleve`
+--
+
+LOCK TABLES `eleve` WRITE;
+/*!40000 ALTER TABLE `eleve` DISABLE KEYS */;
+INSERT INTO `eleve` VALUES (1,2018),(3,2018),(4,2018),(5,2018);
+/*!40000 ALTER TABLE `eleve` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `enonce`
+--
+
+DROP TABLE IF EXISTS `enonce`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `enonce` (
+  `idEnonce` int(11) NOT NULL,
+  `titre` varchar(50) NOT NULL,
+  `consigne` text NOT NULL,
+  PRIMARY KEY (`idEnonce`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `enonce`
+--
+
+LOCK TABLES `enonce` WRITE;
+/*!40000 ALTER TABLE `enonce` DISABLE KEYS */;
+INSERT INTO `enonce` VALUES (1,'Simulation de fusée','Selon les paramètres suivants, indiquez les quantités nécessaires d\'O2, de carburant, de nourriture et d\'eau pour atteindre [[planète]] :\r\n-nombre de moteurs : [[nbMoteur]]\r\n-vitesse : [[vitesseMoteur]]\r\n-Consommation de carburant par moteur : 100T/1000km\r\n-Consommation d\'eau par jour par personne : 1,5L\r\n-Consommation de nourriture par personne par journée : 2 Kg\r\n-Consommation d\'O² par personne par jour : 60L\r\n-Le nombre de personnes dans l\'équipage : [[nbPersonne]]\r\n-Destination : [[destinationPlanete]]\r\n-Distance Terre/[[destinationPlanete]] : [[distanceDestination]]');
+/*!40000 ALTER TABLE `enonce` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `enseignant`
+--
+
+DROP TABLE IF EXISTS `enseignant`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `enseignant` (
+  `idEnseignant` int(11) NOT NULL,
+  PRIMARY KEY (`idEnseignant`),
+  CONSTRAINT `enseignant_ibfk_1` FOREIGN KEY (`idEnseignant`) REFERENCES `personne` (`idPersonne`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `enseignant`
+--
+
+LOCK TABLES `enseignant` WRITE;
+/*!40000 ALTER TABLE `enseignant` DISABLE KEYS */;
+INSERT INTO `enseignant` VALUES (2);
+/*!40000 ALTER TABLE `enseignant` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `examen`
+--
+
+DROP TABLE IF EXISTS `examen`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `examen` (
+  `idExamen` int(11) NOT NULL AUTO_INCREMENT,
+  `dateDepot` datetime NOT NULL,
+  `anneeScolaire` int(4) NOT NULL,
+  PRIMARY KEY (`idExamen`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `examen`
+--
+
+LOCK TABLES `examen` WRITE;
+/*!40000 ALTER TABLE `examen` DISABLE KEYS */;
+INSERT INTO `examen` VALUES (1,'2018-11-30 00:00:00',2018);
+/*!40000 ALTER TABLE `examen` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `exerciceattribue`
+--
+
+DROP TABLE IF EXISTS `exerciceattribue`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `exerciceattribue` (
+  `idEleve` int(11) NOT NULL,
+  `idSujet` int(11) NOT NULL,
+  PRIMARY KEY (`idEleve`,`idSujet`),
+  KEY `idSujet` (`idSujet`),
+  CONSTRAINT `exerciceattribue_ibfk_1` FOREIGN KEY (`idEleve`) REFERENCES `eleve` (`idEleve`),
+  CONSTRAINT `exerciceattribue_ibfk_2` FOREIGN KEY (`idSujet`) REFERENCES `sujet` (`idSujet`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `exerciceattribue`
+--
+
+LOCK TABLES `exerciceattribue` WRITE;
+/*!40000 ALTER TABLE `exerciceattribue` DISABLE KEYS */;
+/*!40000 ALTER TABLE `exerciceattribue` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `exercicegenere`
+--
+
+DROP TABLE IF EXISTS `exercicegenere`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `exercicegenere` (
+  `idSujet` int(11) NOT NULL,
+  `idValeur` int(11) NOT NULL,
+  PRIMARY KEY (`idSujet`,`idValeur`),
+  KEY `idValeur` (`idValeur`),
+  CONSTRAINT `exercicegenere_ibfk_1` FOREIGN KEY (`idSujet`) REFERENCES `sujet` (`idSujet`),
+  CONSTRAINT `exercicegenere_ibfk_2` FOREIGN KEY (`idValeur`) REFERENCES `valeurs` (`idValeur`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `exercicegenere`
+--
+
+LOCK TABLES `exercicegenere` WRITE;
+/*!40000 ALTER TABLE `exercicegenere` DISABLE KEYS */;
+INSERT INTO `exercicegenere` VALUES (1,1),(2,1),(1,6),(2,6),(1,11),(2,14),(1,21),(2,21),(1,23),(2,27),(1,39),(2,39),(1,40),(2,40),(1,41),(2,41),(1,42),(2,42);
+/*!40000 ALTER TABLE `exercicegenere` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `note`
+--
+
+DROP TABLE IF EXISTS `note`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `note` (
+  `idEleve` int(11) NOT NULL,
+  `idSujet` int(11) NOT NULL,
+  `note` decimal(4,2) NOT NULL,
+  PRIMARY KEY (`idEleve`,`idSujet`),
+  KEY `idSujet` (`idSujet`),
+  CONSTRAINT `note_ibfk_1` FOREIGN KEY (`idEleve`) REFERENCES `eleve` (`idEleve`),
+  CONSTRAINT `note_ibfk_2` FOREIGN KEY (`idSujet`) REFERENCES `sujet` (`idSujet`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `note`
+--
+
+LOCK TABLES `note` WRITE;
+/*!40000 ALTER TABLE `note` DISABLE KEYS */;
+/*!40000 ALTER TABLE `note` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `personne`
+--
+
+DROP TABLE IF EXISTS `personne`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `personne` (
+  `idPersonne` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(30) NOT NULL,
+  `prenom` varchar(30) NOT NULL,
+  `login` varchar(30) NOT NULL,
+  `mdp` varchar(30) NOT NULL,
+  PRIMARY KEY (`idPersonne`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `personne`
+--
+
+LOCK TABLES `personne` WRITE;
+/*!40000 ALTER TABLE `personne` DISABLE KEYS */;
+INSERT INTO `personne` VALUES (1,'Dupont','Jean','Dupont.Jean','tintin'),(2,'Poitou','Nicolas','Poitou.Nicolas','charente'),(3,'Potter','Harry','Potter.Harry','dumbledore'),(4,'McQueen','Flash','McQueen.Flash','vitesse'),(5,'Sparrow','Jack','Sparrow.Jack','pirate');
+/*!40000 ALTER TABLE `personne` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `points`
+--
+
+DROP TABLE IF EXISTS `points`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `points` (
+  `idPoint` int(11) NOT NULL,
+  `idExamen` int(11) NOT NULL,
+  `nomPoint` varchar(50) NOT NULL,
+  `unitePoint` varchar(30) NOT NULL,
+  PRIMARY KEY (`idPoint`,`idExamen`),
+  KEY `idExamen` (`idExamen`),
+  CONSTRAINT `points_ibfk_1` FOREIGN KEY (`idExamen`) REFERENCES `examen` (`idExamen`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `points`
+--
+
+LOCK TABLES `points` WRITE;
+/*!40000 ALTER TABLE `points` DISABLE KEYS */;
+INSERT INTO `points` VALUES (1,1,'nbMoteur','unité'),(2,1,'vitesseMoteur','Km/H'),(3,1,'nbPersonne','unité'),(4,1,'destinationPlanète',''),(5,1,'distanceDestination','milllions de Km'),(6,1,'consoCarburantParMoteurs','tonnes/1000km'),(7,1,'consoEauParPersonnesParJour','L'),(8,1,'consoNourrituresParPersonneParJour','KG'),(9,1,'consoO2ParPersonnesParJour','L');
+/*!40000 ALTER TABLE `points` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `resultatsattendus`
+--
+
+DROP TABLE IF EXISTS `resultatsattendus`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `resultatsattendus` (
+  `idSujet` int(11) NOT NULL,
+  `idReponse` int(11) NOT NULL,
+  `resultat` decimal(5,1) NOT NULL,
+  `bareme` decimal(4,2) NOT NULL,
+  PRIMARY KEY (`idReponse`,`idSujet`),
+  KEY `idSujet` (`idSujet`),
+  CONSTRAINT `resultatsattendus_ibfk_1` FOREIGN KEY (`idSujet`) REFERENCES `sujet` (`idSujet`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `resultatsattendus`
+--
+
+LOCK TABLES `resultatsattendus` WRITE;
+/*!40000 ALTER TABLE `resultatsattendus` DISABLE KEYS */;
+/*!40000 ALTER TABLE `resultatsattendus` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `resultatseleves`
+--
+
+DROP TABLE IF EXISTS `resultatseleves`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `resultatseleves` (
+  `dateResult` datetime NOT NULL,
+  `idEleve` int(11) NOT NULL,
+  `idSujet` int(11) NOT NULL,
+  `idReponse` int(11) NOT NULL,
+  `resultat` decimal(5,1) NOT NULL,
+  `precisionReponse` decimal(4,2) NOT NULL,
+  PRIMARY KEY (`dateResult`,`idEleve`,`idSujet`),
+  KEY `idEleve` (`idEleve`),
+  KEY `idSujet` (`idSujet`),
+  KEY `idReponse` (`idReponse`),
+  CONSTRAINT `resultatseleves_ibfk_1` FOREIGN KEY (`idEleve`) REFERENCES `eleve` (`idEleve`),
+  CONSTRAINT `resultatseleves_ibfk_2` FOREIGN KEY (`idSujet`) REFERENCES `sujet` (`idSujet`),
+  CONSTRAINT `resultatseleves_ibfk_3` FOREIGN KEY (`idReponse`) REFERENCES `resultatsattendus` (`idReponse`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `resultatseleves`
+--
+
+LOCK TABLES `resultatseleves` WRITE;
+/*!40000 ALTER TABLE `resultatseleves` DISABLE KEYS */;
+/*!40000 ALTER TABLE `resultatseleves` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `schemasujet`
+--
+
+DROP TABLE IF EXISTS `schemasujet`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `schemasujet` (
+  `idSujet` int(11) NOT NULL,
+  `schema1` longblob NOT NULL,
+  `schema2` longblob NOT NULL,
+  PRIMARY KEY (`idSujet`),
+  CONSTRAINT `schemasujet_ibfk_1` FOREIGN KEY (`idSujet`) REFERENCES `sujet` (`idSujet`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `schemasujet`
+--
+
+LOCK TABLES `schemasujet` WRITE;
+/*!40000 ALTER TABLE `schemasujet` DISABLE KEYS */;
+/*!40000 ALTER TABLE `schemasujet` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `sujet`
+--
+
+DROP TABLE IF EXISTS `sujet`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `sujet` (
+  `idSujet` int(11) NOT NULL AUTO_INCREMENT,
+  `idEnonce` int(11) NOT NULL,
+  `semestre` tinyint(1) NOT NULL,
+  `idExamen` int(11) NOT NULL,
+  PRIMARY KEY (`idSujet`,`idEnonce`),
+  KEY `idEnonce` (`idEnonce`),
+  KEY `idExamen` (`idExamen`),
+  CONSTRAINT `sujet_ibfk_1` FOREIGN KEY (`idEnonce`) REFERENCES `enonce` (`idEnonce`),
+  CONSTRAINT `sujet_ibfk_2` FOREIGN KEY (`idExamen`) REFERENCES `examen` (`idExamen`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sujet`
+--
+
+LOCK TABLES `sujet` WRITE;
+/*!40000 ALTER TABLE `sujet` DISABLE KEYS */;
+INSERT INTO `sujet` VALUES (1,1,1,1),(2,1,1,1);
+/*!40000 ALTER TABLE `sujet` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `valeurs`
+--
+
+DROP TABLE IF EXISTS `valeurs`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `valeurs` (
+  `idValeur` int(11) NOT NULL,
+  `idPoint` int(11) NOT NULL,
+  `valeur` varchar(50) NOT NULL,
+  PRIMARY KEY (`idValeur`),
+  KEY `idPoint` (`idPoint`),
+  CONSTRAINT `valeurs_ibfk_1` FOREIGN KEY (`idPoint`) REFERENCES `points` (`idPoint`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `valeurs`
+--
+
+LOCK TABLES `valeurs` WRITE;
+/*!40000 ALTER TABLE `valeurs` DISABLE KEYS */;
+INSERT INTO `valeurs` VALUES (1,1,'1'),(2,1,'2'),(3,1,'3'),(4,1,'4'),(5,1,'5'),(6,2,'1000'),(7,2,'2000'),(8,2,'3000'),(9,2,'4000'),(10,2,'5000'),(11,3,'3'),(12,3,'4'),(13,3,'5'),(14,3,'6'),(15,3,'7'),(16,3,'8'),(17,3,'9'),(18,3,'10'),(19,3,'11'),(20,3,'12'),(21,4,'Lune'),(22,4,'Mars'),(23,5,'340000'),(24,5,'350000'),(25,5,'360000'),(26,5,'370000'),(27,5,'380000'),(28,5,'390000'),(29,5,'400000'),(30,5,'410000'),(31,5,'50000000'),(32,5,'100000000'),(33,5,'150000000'),(34,5,'200000000'),(35,5,'250000000'),(36,5,'300000000'),(37,5,'350000000'),(38,5,'400000000'),(39,6,'100'),(40,7,'1.5'),(41,8,'2'),(42,9,'60');
+/*!40000 ALTER TABLE `valeurs` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-11-21 14:05:43
