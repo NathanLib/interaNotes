6696155d3a0bf19f2546f96687c5ce887a4df81c
Nathan Liboutet <nathan.lib19@gmail.com>
2018-11-24 17:56:44 +0100

    début créer classe

---
+++ interaNotes/include/pages/prof_creerClasse.inc.php
@@ -0,0 +1,20 @@
+<div class="row justify-content-around">
+    <div class="col-8 col-sm-6">
+        <label for="">Nom : </label>
+        <input type="text" name="" value="" placeholder="Promo 1A" size="25px">
+    </div>
+
+    <div class="col-8 col-sm-6">
+        <label for="">Année : </label>
+        <input type="text" name="" value="" placeholder="20../20.." size="25px">
+    </div>
+</div>
+
+<div class="row justify-conten-around">
+    <div class="col-2">
+        <img class="" src="image/plus.png" alt="plus" title="plus" width="40px">
+    </div>
+    <div class="col-7">
+        <p>Importer un fichier d'élèves</p>
+    </div>
+</div>
