3542caae41621e28cec534b593df14c0f5c79251
Leger Charlie <charlie.leger@etu.unilim.fr>
2018-11-29 11:26:20 +0100

    fix formule

--- interaNotes/include/pages/test_afficherUnSujetCorrige.inc.php
+++ interaNotes/include/pages/test_afficherUnSujetCorrige.inc.php
@@ -1,6 +1,5 @@
-<?php 
+<?php
 $db = new Mypdo();
-$formuleManager = new FormuleManager($db);
 $corrigeManager = new CorrigeManager($db);
 echo "<pre>";var_dump($corrigeManager->calculerCorrection($_GET['id']));echo "</pre>";
 
