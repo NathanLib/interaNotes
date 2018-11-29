5b1d6d18160e2eba91e69e70112991bdfa3fc6d7
Nathan Liboutet <nathan.lib19@gmail.com>
2018-11-29 16:06:10 +0100

    ajout design input file

--- interaNotes/js/main.js
+++ interaNotes/js/main.js
@@ -1,11 +1,41 @@
 $(document).ready(function() {
-  function setHeight() {
-    windowHeight = $(window).innerHeight();
-    $('.container-fluid').css('min-height', windowHeight-($('footer').height() + $('nav').height() + 26));
-  };
-  setHeight();
-
-  $(window).resize(function() {
+    function setHeight() {
+        windowHeight = $(window).innerHeight();
+        $('.container-fluid').css('min-height', windowHeight-($('footer').height() + $('nav').height() + 26));
+    };
     setHeight();
-  });
+
+    $(window).resize(function() {
+        setHeight();
+    });
 });
+
+'use strict';
+
+;( function ( document, window, index )
+{
+    var inputs = document.querySelectorAll( '.inputfile' );
+    Array.prototype.forEach.call( inputs, function( input )
+    {
+        var label	 = input.nextElementSibling,
+        labelVal = label.innerHTML;
+
+        input.addEventListener( 'change', function( e )
+        {
+            var fileName = '';
+            if( this.files && this.files.length > 1 )
+            fileName = ( this.getAttribute( 'data-multiple-caption' ) || '' ).replace( '{count}', this.files.length );
+            else
+            fileName = e.target.value.split( '\\' ).pop();
+
+            if( fileName )
+            label.querySelector( 'span' ).innerHTML = fileName;
+            else
+            label.innerHTML = labelVal;
+        });
+
+        // Firefox bug fix
+        input.addEventListener( 'focus', function(){ input.classList.add( 'has-focus' ); });
+        input.addEventListener( 'blur', function(){ input.classList.remove( 'has-focus' ); });
+    });
+}( document, window, 0 ));
