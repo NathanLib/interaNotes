f4078f229c063dc05878f31690a6cbb6f7e159dd
Kant1 <quentin.lenoir@etu.unilim.fr>
2018-11-30 22:00:41 +0100

    indentation page Connexion

--- interaNotes/include/pages/Connexion.inc.php
+++ interaNotes/include/pages/Connexion.inc.php
@@ -1,9 +1,9 @@
 <?php
 if (isset($_SESSION['eleve'])){
-     header('Location: index.php?page=7');
- } elseif (isset($_SESSION['prof'])) {
-      header('Location: index.php?page=1');
- }
+   header('Location: index.php?page=7');
+} elseif (isset($_SESSION['prof'])) {
+  header('Location: index.php?page=1');
+}
 
 
 if(empty($_POST['mdp'])) { ?>
@@ -41,64 +41,64 @@ if(empty($_POST['mdp'])) { ?>
         </div>
     <?php } else {
 
-       $db = new Mypdo();
-       $personneManager = new PersonneManager($db);
+     $db = new Mypdo();
+     $personneManager = new PersonneManager($db);
 
-       $log = $personneManager->connexion($_POST['login'],$_POST['mdp']);
+     $log = $personneManager->connexion($_POST['login'],$_POST['mdp']);
 
-       switch ($log) {
-           case 0:
-                ?>
-               <div class="row justify-content-center contenuConnexion">
-                <div class="col-9 col-md-6">
-                    <div class="row justify-content-center">
-                        <div class="col-sm-5 logoConnexion">
-                            <img class="logoConnexion" src="image/board.png" alt="logo" title="logo">
-                        </div>
-                        <div class="col-sm-7 text-center interaNoteConnexion">
-                            <h2>Intera Note</h2>
-                        </div>
+     switch ($log) {
+         case 0:
+         ?>
+         <div class="row justify-content-center contenuConnexion">
+            <div class="col-9 col-md-6">
+                <div class="row justify-content-center">
+                    <div class="col-sm-5 logoConnexion">
+                        <img class="logoConnexion" src="image/board.png" alt="logo" title="logo">
                     </div>
+                    <div class="col-sm-7 text-center interaNoteConnexion">
+                        <h2>Intera Note</h2>
+                    </div>
+                </div>
 
-                    <!--<form action="#" method="post">-->
-                        <form action="index.php?page=0" method="post">
-                            <p><img src="image/erreur.png" alt="Erreur" title="Erreur" /> Login / Mot de passe incorrect</p>
-                            <div class="row">
-                                <div class="col-12">
-                                    <input class="inputConnexion" type="text" name="login" placeholder="Identifiant">
-                                </div>
+                <!--<form action="#" method="post">-->
+                    <form action="index.php?page=0" method="post">
+                        <p><img src="image/erreur.png" alt="Erreur" title="Erreur" /> Login / Mot de passe incorrect</p>
+                        <div class="row">
+                            <div class="col-12">
+                                <input class="inputConnexion" type="text" name="login" placeholder="Identifiant">
                             </div>
-                            <div class="row">
-                                <div class="col-12">
-                                    <input class="inputConnexion" type="password" name="mdp" placeholder="Mot de passe">
-                                </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-12">
+                                <input class="inputConnexion" type="password" name="mdp" placeholder="Mot de passe">
                             </div>
+                        </div>
 
-                            <div class="row buttonConnexion">
-                                <div class="col-7">
-                                    <input type="submit" name="submit" value="Connexion">
-                                </div>
+                        <div class="row buttonConnexion">
+                            <div class="col-7">
+                                <input type="submit" name="submit" value="Connexion">
                             </div>
-                        </form>
+                        </div>
+                    </form>
+                </div>
             </div>
-        </div>
 
 
-          <?php    
-           break;
+            <?php    
+            break;
             
             case 1:
-               $_SESSION['eleve']=$_POST['login'];
-               header('Location: index.php?page=7');
-               break;
+            $_SESSION['eleve']=$_POST['login'];
+            header('Location: index.php?page=7');
+            break;
 
             case 2:
-               $_SESSION['prof']=$_POST['login'];
-               header('Location: index.php?page=1');
-               break;
-           
-           default:
-               header('Location: index.php?page=0');
-               break;
-       }
-   } ?>
+            $_SESSION['prof']=$_POST['login'];
+            header('Location: index.php?page=1');
+            break;
+            
+            default:
+            header('Location: index.php?page=0');
+            break;
+        }
+    } ?>
