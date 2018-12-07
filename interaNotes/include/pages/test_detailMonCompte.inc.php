2a96ad5744fe6edb68821b09f362cd4c6d8a6be4
Nathan Liboutet <nathan.lib19@gmail.com>
2018-12-07 22:37:51 +0100

    add desktop version details

--- interaNotes/include/pages/test_detailMonCompte.inc.php
+++ interaNotes/include/pages/test_detailMonCompte.inc.php
@@ -1,20 +1,20 @@
 <div class="row">
-    <div class="col-12">
-        <div class="row">
-            <div class="col-12 imgDetailsCompte">
+    <div class="col-12 col-lg-6">
+        <div class="row headDetailCompte">
+            <div class="col-12 col-sm-4 col-lg-12 imgDetailsCompte">
                 <div class="iconStudentDetailsCompte">
                     <img class="studentImg" src="image/student.png" alt="subject" title="subject">
                 </div>
             </div>
-            <div class="col-12 nameDetailsCompte">
+            <div class="col-12 col-sm-6 col-lg-12 nameDetailsCompte">
                 <h3>Harry Potter</h3>
             </div>
         </div>
     </div>
 
-    <hr class="hr-DetailsCompte">
+    <hr class="hr-DetailsCompte d-lg-none">
 
-    <div class="col-12">
+    <div class="col-12 col-lg-6 bodyDetailCompte">
         <div class="row">
             <div class="col-12 partDetailCompte">
                 <span class="titleDetailCompte">Nom de la promotion :</span>
@@ -24,11 +24,7 @@
                 <span class="titleDetailCompte">Année :</span>
                 <span class="answerDetailCompte">2018</span>
             </div>
-        </div>
-    </div>
 
-    <div class="col-12">
-        <div class="row">
             <div class="col-12 partDetailCompte">
                 <span class="titleDetailCompte">Mail :</span>
                 <span class="answerDetailCompte">harry.potter@gmail.com</span>
