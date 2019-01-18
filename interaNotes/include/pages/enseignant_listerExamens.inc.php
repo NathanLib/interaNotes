09c21b42bd7767cc711a90de4b836285a6a9df34
Leger Charlie <charlie.leger@etu.unilim.fr>
2019-01-18 13:23:46 +0100

    refactor methode3 generation

---
+++ interaNotes/include/pages/enseignant_listerExamens.inc.php
@@ -0,0 +1,53 @@
+<?php require_once("include/verifEnseignant.inc.php"); ?>
+
+<?php
+$pdo = new Mypdo();
+$examenManager = new ExamenManager($pdo);
+
+$listeExamens = $sujetManager->getAllSujetsOfExamenAttribues($_SESSION['examen']->getIdExamen());
+
+
+if (!$listeSujets){ ?>
+    <div class="msgErrorTitre">
+        <h3>Erreur sujet</h3>
+        <p>Aucun sujet n'a été attribué pour l'instant !</p>
+    </div>
+<?php } else { ?>
+
+    <div class="listerSujet">
+        <?php // WARNING: BLOC A LAISSER ?>
+        <div class="row justify-content-around text-center teteListeSujet">
+            <div class="col-6 col-sm-3 col-lg-2 textListeSujet">
+                <p> </p>
+            </div>
+            <div class="col-6 col-lg-4">
+                <span id="attributeTo">Attribué à : </span>
+            </div>
+            <div class="col-3 col-lg-2">
+            </div>
+        </div>
+
+        <?php // WARNING: BLOC A GENERER EN PHP
+        foreach ($listeSujets as $sujet) { ?>
+            <div class="row justify-content-center text-center contenuListeSujet">
+                <div class="col-6 col-sm-3 col-lg-2 textListeSujet">
+                    <p>Sujet n°<?php echo $sujet->getIdSujet() ?></p>
+                </div>
+
+                <?php $eleve = $personneManager->getNomPrenomParSujet($sujet->getIdSujet()); ?>
+
+                <div class="col-6 col-lg-4 textListeSujet">
+                    <p> <?php echo $eleve->getPrenomPersonne()." ".$eleve->getNomPersonne() ?></p>
+                </div>
+
+                <div class="col-6 col-sm-3 col-lg-2 buttonConsulter">
+                    <a href="index.php?page=20&amp;id=<?php echo $sujet->getIdSujet();?>">
+                        <input type="button" name="" value="Consulter">
+                    </a>
+                </div>
+            </div>
+        <?php }
+    }
+    ?>
+
+</div>
