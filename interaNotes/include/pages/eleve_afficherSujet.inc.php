b3760f470a9eb31afecf74f1618f4fce196fbd98
Leger Charlie <charlie.leger@etu.unilim.fr>
2019-02-06 22:21:52 +0100

    renommage du pdf

--- interaNotes/include/pages/eleve_afficherSujet.inc.php
+++ interaNotes/include/pages/eleve_afficherSujet.inc.php
@@ -170,7 +170,7 @@ if (!$idSujet){
                     <?php
                     $_SESSION['sujet'] = $arrayName = array('idSujet' => $idSujet,'titre' => $titre, 'date' => $date, 'enonce' => $enonce,'image1' => $image1, 'image2' => $image2);
                     ?>
-                    <a href="include/pages/test_pdf.inc.php" target="_blank">
+                    <a href="include/pages/obtenirPdfSujet.inc.php" target="_blank">
                         <input type=button value="Télécharger"></input>
                     </a>
                 </div>
