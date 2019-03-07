f3e81ae004ebf991681c3d0c8276022cac9bc6d2
Leger Charlie <charlie.leger@etu.unilim.fr>
2019-03-07 12:52:29 +0100

    fix afficher un sujet

--- interaNotes/classes/QuestionManager.class.php
+++ interaNotes/classes/QuestionManager.class.php
@@ -7,13 +7,14 @@ class QuestionManager{
 		$this->db = $db;
 	}
 
-  public function getAllQuestion($idSujet){
+  public function getAllQuestion($idExamen, $idSujet){
     $sql = 'SELECT q.idQuestion, intituleQuestion, resultat, exposantUnite, resultatUnite, baremeQuestion , resultatExposant, zoneTolerance FROM resultatsattendus r
     INNER JOIN Question q ON (q.idQuestion=r.idQuestion)
-    WHERE idSujet=:idSujet';
+    WHERE idSujet=:idSujet AND idExamen=:idExamen';
 
     $requete = $this->db->prepare($sql);
     $requete->bindValue(':idSujet', $idSujet);
+    $requete->bindValue(':idExamen', $idExamen);
     $requete->execute();
 
     while($question = $requete->fetch(PDO::FETCH_OBJ)){
