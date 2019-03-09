0ce59f551d2e51a3a4c50a7e828163a74c0d3c33
Jérémy <jeremy.lorthioir24@gmail.com>
2019-03-09 10:01:14 +0100

    Suppression colone précision
    Afficher sujet si date de fin non dépassée

--- interaNotes/include/pages/afficherSujet_avecResultats.inc.php
+++ interaNotes/include/pages/afficherSujet_avecResultats.inc.php
@@ -25,7 +25,7 @@ if(isset($idSujet)) {
 			<h4 style="margin-top:30px">Résultats attendus : </h4>
 			<p>La date de dépôt du sujet n'est pas encore passée</p>
 
-		<?php
+			<?php
 		}else{ ?>
 			<h4 style="margin-top:30px">Résultats attendus : </h4>
 			<div class="row d-flex justify-content-center correctionSujet">
@@ -80,53 +80,57 @@ if(isset($idSujet)) {
 			<?php
 		} ?>
 
-			<h4> Résultats saisis par l'étudiant : </h4>
+		<h4> Résultats saisis par l'étudiant : </h4>
+
+		<?php
+		if(!$listeReponses){ ?>
+
+			<div class="msgErrorTitre">
+				<h3>Erreur saisie</h3>
+				<p>Aucune réponse saisie par l'étudiant !</p>
+			</div>
 
 			<?php
-			if(!$listeReponses){ ?>
+		} else { ?>
+			<div class="col-12 listImportStudent">
 
-				<div class="msgErrorTitre">
-			        <h3>Erreur saisie</h3>
-			        <p>Aucune réponse saisie par l'étudiant !</p>
-			    </div>
-
-			<?php
-			} else { ?>
-				<div class="col-12 listImportStudent">
-
-					<table class="table table-hover">
-						<thead class="thead-dark">
-							<tr>
-								<th scope="col" style="border-radius: 20px 0 0 0;">Date de saisie</th>
-								<th scope="col">Question n°</th>
-								<th scope="col">Résultat saisi</th>
-								<th scope="col">Exposant du résultat</th>
-								<th scope="col">Unité</th>
-								<th scope="col">Exposant de l'unité</th>
-								<th scope="col">Justification</th>
+				<table class="table table-hover">
+					<thead class="thead-dark">
+						<tr>
+							<th scope="col" style="border-radius: 20px 0 0 0;">Date de saisie</th>
+							<th scope="col">Question n°</th>
+							<th scope="col">Résultat saisi</th>
+							<th scope="col">Exposant du résultat</th>
+							<th scope="col">Unité</th>
+							<th scope="col">Exposant de l'unité</th>
+							<th scope="col" <?php if (!$examenSujet->estFini()){ ?> style="border-radius: 0 20px 0 0;"<?php } ?> >Justification</th>
+							<?php if ($examenSujet->estFini()){ ?>
 								<th scope="col" style="border-radius: 0 20px 0 0;">Précision</th>
-							</tr>
-						</thead>
-
-						<tbody>
-							<?php foreach ($listeReponses as $reponse) { ?>
-								<tr>
-									<td><?php echo getFrenchDate($reponse->getDateResult()) ?></td>
-									<td><?php echo $reponse->getIdQuestion() ?></td>
-									<td><?php echo $reponse->getResultat() ?></td>
-									<td><?php echo $reponse->getResultatUnite() ?></td>
-									<td><?php echo $reponse->getExposantUnite() ?></td>
-									<td><?php echo $reponse->getResultatExposant() ?></td>
-									<td style="overflow-wrap: break-word;max-width:400px"><?php echo $reponse->getJustification() ?></td>
-									<td><?php echo $reponse->getPrecisionReponse() ?> %</td>
-								</tr>
 							<?php } ?>
-						</tbody>
-					</table>
-				</div>
+						</tr>
+					</thead>
+
+					<tbody>
+						<?php foreach ($listeReponses as $reponse) { ?>
+							<tr>
+								<td><?php echo getFrenchDate($reponse->getDateResult()) ?></td>
+								<td><?php echo $reponse->getIdQuestion() ?></td>
+								<td><?php echo $reponse->getResultat() ?></td>
+								<td><?php echo $reponse->getResultatUnite() ?></td>
+								<td><?php echo $reponse->getExposantUnite() ?></td>
+								<td><?php echo $reponse->getResultatExposant() ?></td>
+								<td style="overflow-wrap: break-word;max-width:400px"><?php echo $reponse->getJustification() ?></td>
+								<?php if ($examenSujet->estFini()){ ?>
+									<td><?php echo $reponse->getPrecisionReponse() ?> %</td>
+								<?php } ?>
+							</tr>
+						<?php } ?>
+					</tbody>
+				</table>
+			</div>
 
 			<?php
-		 	}
+		}
 
 	} else {
 		header('Location: index.php?page=3');
